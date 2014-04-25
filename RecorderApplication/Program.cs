b779c1b1b5e029ddce3f7865f131d76703282222
Ben Lings <ben.lings@gmail.com>
2014-04-25 12:28:23 +0100

    Add wrapper application

---
+++ RecorderApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using CodaRecorder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecorderApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recorder Application");
+
+            var recorder = new Recorder();
+            Console.WriteLine("{0} Entries", recorder.KeyCount());
+            Console.ReadLine();
+        }
+    }
+}
