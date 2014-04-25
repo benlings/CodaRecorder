211452f9b7ca49673daf2a0fe59e2a0235c3e442
Tom Venn <thomas.venn@tessella.com>
2014-04-25 11:42:39 +0100

    Add project and test project

---
+++ CodaRecorder/IMutableRecorder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodaRecorder
+{
+    interface IMutableRecorder
+    {
+
+        void Upsert(String key, int value);
+
+        void Delete(String key);
+    }
+}
