1999aa57b22ac1ce388a0e1fda9aa4c664afab66
Tom Venn <thomas.venn@tessella.com>
2014-04-25 12:18:46 +0100

    Put Recorder and Tests in place. Added NUnit library to fix references.

---
+++ CodaRecorder/Recorder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodaRecorder
+{
+    public class Recorder : IMutableRecorder
+    {
+        int count = 0;
+
+        public int KeyCount()
+        {
+            return this.count;
+        }
+
+        public int Get(string key)
+        {
+            return 1;
+        }
+
+        public void Upsert(string key, int value)
+        {
+            this.count++;
+        }
+
+        public void Delete(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodaRecorderTest/RecorderTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodaRecorder
+{
+    [TestFixture]
+    public class RecorderTest
+    {
+        [Test]
+        public void new_recorder_key_count_is_zero()
+        {
+            Recorder testRecorder = new Recorder();
+
+            Assert.That(testRecorder.KeyCount(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void insert_new_key_increments_key_count()
+        {
+            Recorder testRecorder = new Recorder();
+            testRecorder.Upsert("testKey", 1);
+
+            Assert.That(testRecorder.KeyCount(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void requesting_inserted_key_gives_associated_value()
+        {
+            Recorder testRecorder = new Recorder();
+            testRecorder.Upsert("testKey", 1);
+
+            Assert.That(testRecorder.Get("testKey"), Is.EqualTo(1));
+
+        }
+    }
+}
