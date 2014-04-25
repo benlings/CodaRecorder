08c63aef0c4a4331466cbd5e849baffb81e65589
Ben Lings <ben.lings@gmail.com>
2014-04-25 13:29:04 +0100

    refactor: Extract ParseValueCommand method

--- CodaRecorder/Parser.cs
+++ CodaRecorder/Parser.cs
@@ -14,23 +14,28 @@ namespace CodaRecorder
             switch (parts[0])
             {
                 case "value":
-                    if (parts.Length != 3)
-                    {
-                        return new Invalid("Incorrectly formatted value string");
-                    }
+                    return ParseValueCommand(parts);
 
-                    int val;
-                    if (int.TryParse(parts[2], out val))
-                    {
-                        return new Upsert(parts[1], val);
-                    }
-                    else
-                    {
-                        return new Invalid("Non-integer value");
-                    }
-                    
                 default:
                     return new Invalid("Unknown command");
+            }
+        }
+
+        private static Command ParseValueCommand(string[] parts)
+        {
+            if (parts.Length != 3)
+            {
+                return new Invalid("Incorrectly formatted value string");
+            }
+
+            int val;
+            if (int.TryParse(parts[2], out val))
+            {
+                return new Upsert(parts[1], val);
+            }
+            else
+            {
+                return new Invalid("Non-integer value");
             }
         }
     }
